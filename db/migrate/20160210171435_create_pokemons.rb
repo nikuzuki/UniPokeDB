f23693da4c2c7d8950d437d4ed8b22f0e1a0dccb
Kohei Makita <nikuzuki29@gmail.com>
2016-02-11 03:10:33 +0900

    migrate database
    データベースを作った

--- db/migrate/20160210171435_create_pokemons.rb
+++ db/migrate/20160210171435_create_pokemons.rb
@@ -2,9 +2,19 @@ class CreatePokemons < ActiveRecord::Migration[5.0]
   def change
     create_table :pokemons do |t|
       t.integer :all_world_number
-      t.string :name
+      t.string  :name
       t.integer :type_1
       t.integer :type_2
+      t.integer :race_h
+      t.integer :race_a
+      t.integer :race_b
+      t.integer :race_c
+      t.integer :race_d
+      t.integer :race_s
+      t.string  :ability_1
+      t.string  :ability_2
+      t.string  :ability_dream
+
 
       t.timestamps
     end
---
+++ db/schema.rb
@@ -0,0 +1,47 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20160210172639) do
+
+  create_table "pokemons", force: :cascade do |t|
+    t.integer  "all_world_number"
+    t.string   "name"
+    t.integer  "type_1"
+    t.integer  "type_2"
+    t.integer  "race_h"
+    t.integer  "race_a"
+    t.integer  "race_b"
+    t.integer  "race_c"
+    t.integer  "race_d"
+    t.integer  "race_s"
+    t.string   "ability_1"
+    t.string   "ability_2"
+    t.string   "ability_dream"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  create_table "types", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "wazas", force: :cascade do |t|
+    t.integer  "type_id"
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+end
